101b08238ec69cc90b8f57d1a7a9e26261210145
Marc Kooij <mrkooij@gmail.com>
2025-04-17 21:14:22 +0200

    first commit

---
+++ make_a_choice.py
@@ -0,0 +1,101 @@
+from random import randint
+import sys
+
+
+def display_welcome_message():
+    """Display a friendly welcome message."""
+    message = (
+        "\nKun je weer niet kiezen? "
+        "Laat dit programma die moeilijke keuze voor je maken."
+    )
+    print(message)
+
+
+def get_number_of_options():
+    """Prompt user for the number of options to choose from."""
+    err = "Kies een getal tussen 2 en 5."
+
+    while True:
+        try:
+            num_options = int(input(
+                "Hoeveel opties wil je vergelijken? (max 5): "
+            ))
+
+            if 1 < num_options <= 5:
+                return num_options
+            else:
+                print(err)
+
+        except ValueError:
+            print(err)
+
+
+def get_user_options(num_options):
+    """Collect user options."""
+    user_options = []
+
+    i = 1
+    while len(user_options) < num_options:
+        if i == 1:
+            option = input(f"Optie {i} ('s' om te stoppen): ")
+        else:
+            option = input(f"Optie {i}: ")
+
+        if option == 's':
+            quit_program()
+        elif option == '' and len(user_options) > 1:
+            break
+        elif option == '':
+            print("Noem minstens twee opties")
+            continue
+
+        user_options.append(option)
+        i += 1
+
+    return user_options
+
+
+def display_user_options(options_list):
+    """Display the entered options."""
+    print("\nDe door u gekozen opties zijn:")
+    for item in options_list:
+        print(f"{options_list.index(item) + 1} - {item}")
+
+
+def determine_choice(options_list):
+    """Determine and display the final choice."""
+    throw = randint(2, 12)
+    i = round((throw / 12) * (len(options_list) - 1))
+
+    print(f"\nHet wordt: {options_list[i]}")
+
+
+def quit_program():
+    """Quit the program gracefully."""
+    sys.exit("Laters!")
+
+
+def restart():
+    """Ask the user for a restart."""
+    prompt = input("Nog een keer proberen? (j/n) ")
+
+    if prompt == 'j':
+        main()
+    else:
+        quit_program()
+
+
+def main():
+    """Main function to run the program."""
+    display_welcome_message()
+
+    num_options = get_number_of_options()
+    user_options = get_user_options(num_options)
+
+    display_user_options(user_options)
+    determine_choice(user_options)
+    restart()
+
+
+if __name__ == "__main__":
+    main()
---
+++ make_a_choice_test.py
@@ -0,0 +1,76 @@
+import make_a_choice
+
+
+# display_welcome_message
+def test_display_welcome_message(capsys):
+    """Test printed result."""
+    make_a_choice.display_welcome_message()
+    captured = capsys.readouterr()
+    assert (
+        "Kun je weer niet kiezen? "
+        "Laat dit programma "
+    ) in captured.out
+
+
+# get_number_of_options
+def test_get_number_of_options_valid(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda _: "2")
+    assert make_a_choice.get_number_of_options() == 2
+
+    monkeypatch.setattr('builtins.input', lambda _: "3")
+    assert make_a_choice.get_number_of_options() == 3
+
+    monkeypatch.setattr('builtins.input', lambda _: "4")
+    assert make_a_choice.get_number_of_options() == 4
+
+    monkeypatch.setattr('builtins.input', lambda _: "5")
+    assert make_a_choice.get_number_of_options() == 5
+
+
+def test_get_number_of_options_non_numerics(monkeypatch, capsys):
+    inputs = iter(["abc", "u", "4"])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    result = make_a_choice.get_number_of_options()
+
+    captured = capsys.readouterr()
+    assert "Kies een getal tussen 2 en 5" in captured.out
+    assert result == 4
+
+
+def test_get_number_of_options_negative_numbers(monkeypatch, capsys):
+    inputs = iter(["-5", "-1", "2"])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    result = make_a_choice.get_number_of_options()
+
+    captured = capsys.readouterr()
+    assert "Kies een getal tussen 2 en 5" in captured.out
+    assert result == 2
+
+
+def test_get_number_of_options_too_small_numbers(monkeypatch, capsys):
+    inputs = iter(["0", "1", "3"])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    result = make_a_choice.get_number_of_options()
+
+    captured = capsys.readouterr()
+    assert "Kies een getal tussen 2 en 5" in captured.out
+    assert result == 3
+
+
+def test_get_number_of_options_too_large_numbers(monkeypatch, capsys):
+    inputs = iter(["6", "10", "4"])
+    monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+    result = make_a_choice.get_number_of_options()
+
+    captured = capsys.readouterr()
+    assert "Kies een getal tussen 2 en 5" in captured.out
+    assert result == 4
+
+
+# get_user_options
+def test_get_user_options():
+    ...
